be4e34f1372b5d9044c95cb38f38c67d7b702331
Dipalipolara <140934647+Dipalipolara@users.noreply.github.com>
2025-12-17 16:14:50 +0530

    bubbul

---
+++ sorting/bubuul.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+// Bubble Sort function
+void sort(int A[], int n)
+{
+    int i, j, temp;
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n - i - 1; j++)
+        {
+            if (A[j] > A[j + 1])
+            {
+                temp = A[j];
+                A[j] = A[j + 1];
+                A[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int A[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+    int n = sizeof(A) / sizeof(A[0]);
+
+    sort(A, n);
+
+    cout << "Sorted Array: ";
+    for (int i = 0; i < n; i++)
+        cout << A[i] << " ";
+
+    return 0;
+}
